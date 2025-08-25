bf769351e5e5369782a980e948e53b47f5c9ed4f
anhvuDev103 <anhvu.dev.103@gmail.com>
2025-08-25 16:58:38 +0700

    Add debug logging to the root endpoint for data fetching

--- index.js
+++ index.js
@@ -10,11 +10,14 @@ app.use(cors());
 
 app.get("/", async (req, res) => {
   try {
+    console.log("1");
     const url =
       "https://www.minhchinh.com/truc-tiep-xo-so-tu-chon-mega-645.html";
+    console.log("1.1");
     const response = await axios.get(url);
     const $ = cheerio.load(response.data);
     const result = [];
+    console.log("2");
 
     $(".mega-645:has(.table-mini-result) tbody tr").each((index, element) => {
       const date = $(element).find("td").eq(0).text().trim();
@@ -39,6 +42,8 @@ app.get("/", async (req, res) => {
         jackpot,
       });
     });
+
+    console.log("3");
 
     res.json(result);
   } catch (error) {
