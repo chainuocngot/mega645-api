8c8046b7f3e723b67685cf2d196fb46b726ee059
anhvuDev103 <anhvu.dev.103@gmail.com>
2025-08-25 17:12:01 +0700

    Comment out data extraction logic in Mega 645 data fetching endpoint

--- index.js
+++ index.js
@@ -12,39 +12,40 @@ app.get("/", async (req, res) => {
   try {
     console.log("1");
     const url =
-      "https://www.minhchinh.com/truc-tiep-xo-so-tu-chon-mega-645.html";
+      "https://www.vietlott.vn/vi/trung-thuong/ket-qua-trung-thuong/winning-number-645";
     console.log("1.1");
     const response = await axios.get(url);
     const $ = cheerio.load(response.data);
     const result = [];
     console.log("2");
 
-    $(".mega-645:has(.table-mini-result) tbody tr").each((index, element) => {
-      const date = $(element).find("td").eq(0).text().trim();
+    // $(".mega-645:has(.table-mini-result) tbody tr").each((index, element) => {
+    //   const date = $(element).find("td").eq(0).text().trim();
 
-      const numbers = $(element)
-        .find(".balls .mini-ball")
-        .map((i, el) => $(el).text().trim())
-        .get();
+    //   const numbers = $(element)
+    //     .find(".balls .mini-ball")
+    //     .map((i, el) => $(el).text().trim())
+    //     .get();
 
-      const jackpot = $(element)
-        .find("td")
-        .eq(2)
-        .text()
-        .replace(/\s+/g, " ")
-        .trim();
+    //   const jackpot = $(element)
+    //     .find("td")
+    //     .eq(2)
+    //     .text()
+    //     .replace(/\s+/g, " ")
+    //     .trim();
 
-      if (!date) return false;
+    //   if (!date) return false;
 
-      result.push({
-        date,
-        numbers,
-        jackpot,
-      });
-    });
+    //   result.push({
+    //     date,
+    //     numbers,
+    //     jackpot,
+    //   });
+    // });
 
-    console.log("3");
+    // console.log("3");
 
+    // res.json(result);
     res.json(result);
   } catch (error) {
     console.error("Lỗi khi lấy dữ liệu:", error.message);
